32d9788acf5b8446f043dc397489d7a030f12f05
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-01-22 10:52:14 +0100

    Enhanced GetValueAsPath().

--- CK.Testing/Configuration/TestHelperConfigurationValue.cs
+++ CK.Testing/Configuration/TestHelperConfigurationValue.cs
@@ -34,16 +34,28 @@ namespace CK.Testing
         /// <summary>
         /// Gets the <see cref="Value"/> as an absolute path or relative to <see cref="BasePath"/>.
         /// If the value is not a valid path, result is what it is, without any warranty.
+        /// The Value can start with '{BinFolder}', {SolutionFolder} or {RepositoryFolder}: <see cref="IBasicTestHelper.BinFolder"/>,
+        /// <see cref="IBasicTestHelper.SolutionFolder"/> or <see cref="IBasicTestHelper.RepositoryFolder"/> is expanded and the suffix
+        /// is resolved (suffixes with \.. are handled).
+        /// Placeholders {BuildConfiguration} and {TestProjectName} can appear anywhere in the Value
+        /// and are replaced with <see cref="IBasicTestHelper.BuildConfiguration"/> and <see cref="IBasicTestHelper.TestProjectName"/>.
         /// </summary>
         /// <returns>The path.</returns>
         public string GetValueAsPath()
         {
             Debug.Assert( Value != null && !BasePath.IsEmpty );
-            var p = Path.IsPathRooted( Value )
-                    ? Path.GetFullPath( Value )
-                    : BasePath.Combine( Value ).ResolveDots().ToString();
-            return p.Replace( "{BuildConfiguration}", BasicTestHelper._buildConfiguration )
-                    .Replace( "{TestProjectName}", BasicTestHelper._testProjectName );
+            NormalizedPath raw;
+            if( Value.StartsWith( "{BinFolder}", StringComparison.OrdinalIgnoreCase ) ) raw = BasicTestHelper._binFolder;
+            else if( Value.StartsWith( "{SolutionFolder}", StringComparison.OrdinalIgnoreCase ) ) raw = BasicTestHelper._solutionFolder;
+            else if( Value.StartsWith( "{RepositoryFolder}", StringComparison.OrdinalIgnoreCase ) ) raw = BasicTestHelper._repositoryFolder;
+            if( raw.IsEmpty )
+            {
+                if( Path.IsPathRooted( Value ) ) raw = Path.GetFullPath( Value );
+                else raw = BasePath.Combine( Value ).ResolveDots();
+            }
+            else raw = raw.ResolveDots();
+            return raw.ToString().Replace( "{BuildConfiguration}", BasicTestHelper._buildConfiguration )
+                                 .Replace( "{TestProjectName}", BasicTestHelper._testProjectName );
         }
 
     }
